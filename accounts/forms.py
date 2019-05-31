ba53f796459da2a854c1716d7caf7ccfe4838d0f
nab5m <sk990420@naver.com>
2019-06-01 02:01:04 +0900

    First Header Design

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 
 from accounts.models import UserProfile
 
@@ -7,3 +9,12 @@ class AccountCreationForm(UserCreationForm):
     class Meta:
         model = UserProfile
         fields = ['username', 'email', 'password1', 'password2', 'address', 'phone', 'birth_date']
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['username', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -3,15 +3,30 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from accounts.forms import AccountCreationForm
-from django.contrib.auth import login
+from accounts.forms import AccountCreationForm, LoginForm
+from django.contrib.auth import login, authenticate
 
 
 def custom_login(request):
-    if request.user.is_authenticated:
+    user = request.user
+    if user.is_authenticated:
         return redirect(reverse('accounts:profile'))
-    else:
-        return login(request)
+    elif request.method == "POST":
+        form = LoginForm(request.POST)
+        user = authenticate(
+            username=form['username'].value(),
+            password=form['password'].value(),
+        )
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('accounts:profile'))
+        else:
+            messages.add_message(request, messages.INFO, "아이디나 비밀번호를 다시 생각해보세요")
+
+    return render(request, 'accounts/login.html', {
+        'user': user,
+        'form': LoginForm(),
+    })
 
 
 def register(request):
@@ -31,7 +46,10 @@ def register(request):
     else:
         form = AccountCreationForm()
 
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, 'accounts/register.html', {
+        'user': user,
+        'form': form,
+    })
 
 
 @login_required(login_url='/accounts/login/')
